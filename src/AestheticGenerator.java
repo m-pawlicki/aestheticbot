e94176799f8b3045abbd801f30869f59ea83eb7b
= <mich.pawlicki@gmail.com>
2017-09-18 15:04:33 -0500

    Refactored to clean code

--- src/AestheticGenerator.java
+++ src/AestheticGenerator.java
@@ -1,10 +1,7 @@
 import com.tumblr.jumblr.JumblrClient;
 import com.tumblr.jumblr.types.TextPost;
-import java.io.IOException;
+
 import java.util.Random;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.Properties;
 
 class AestheticGenerator {
     public static void main(String args[]) {
@@ -15,23 +12,7 @@ class AestheticGenerator {
         String[] noun =
                 {"trash", "dad", "mom", "opossum", "dog", "cat", "meme", "taco", "gay", "lesbian", "bi", "trans", "goth", "punk", "wave", "shoe", "Danny DeVito"};
 
-        Properties auth = new Properties();
-        InputStream input;
-
-        try{
-            input = new FileInputStream("config.properties");
-            auth.load(input);
-        } catch (IOException e){ e.printStackTrace();}
-
-        // Authenticate via OAuth
-        JumblrClient client = new JumblrClient(
-                auth.getProperty("consumerKey"),
-                auth.getProperty("consumerSecret")
-        );
-        client.setToken(
-                auth.getProperty("token"),
-                auth.getProperty("tokenSecret")
-        );
+        JumblrClient client = User.authenticateUser("config.properties");
 
         // Make a new post
         try {
@@ -43,7 +24,7 @@ class AestheticGenerator {
             toPost.addTag("aesthetics");
             toPost.addTag("aestheticgenbot");
             toPost.save();
-        } catch (IllegalAccessException | InstantiationException e){
+        } catch (IllegalAccessException | InstantiationException e) {
             e.printStackTrace();
         }
     }
---
+++ src/User.java
@@ -0,0 +1,36 @@
+import com.tumblr.jumblr.JumblrClient;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+class User {
+
+    public static JumblrClient authenticateUser(String file) {
+
+        //Initialize data
+        Properties auth = new Properties();
+        InputStream input;
+
+        //Open .properties file
+        try {
+            input = new FileInputStream(file);
+            auth.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Authenticate via OAuth
+
+        JumblrClient client = new JumblrClient(
+                auth.getProperty("consumerKey"),
+                auth.getProperty("consumerSecret")
+        );
+
+        client.setToken(
+                auth.getProperty("token"),
+                auth.getProperty("tokenSecret")
+        );
+        return client;
+    }
+}
